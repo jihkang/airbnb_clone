1a445327f5ab7baf5dedcc119d77d700fdae2ca7
강지호 <gangjiho@gangjihoui-MacBookPro.local>
2025-01-27 17:49:49 +0900

    tweet model refact

--- tweet/models.py
+++ tweet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from users.models import CustomUser
 from config import settings
+from django.utils.functional import cached_property
 
 
 class CustomModel(models.Model):
@@ -22,25 +22,30 @@ class Like(CustomModel):
     tweet = models.ForeignKey(
         'Tweet',
         on_delete=models.CASCADE,
-        related_name='likes_set'
+        related_name='tweet_likes'
     )
 
     class Meta:
+        indexes = [
+            models.Index(fields=['user', 'tweet']),
+        ]
         unique_together = (("user", "tweet"))
 
 # Create your models here.
 class Tweet(CustomModel):
     """ tweet model """
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="tweets")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tweets")
     content = models.TextField(max_length=100)
     liked_by = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         through='Like',
         related_name='liked_tweets'
     )
-
+    
+    @cached_property
     def likes_count(self):
         return self.liked_by.count()
 
     def __str__(self):
-        return f"{self.content}"
+        return f"{self.content.strip()}"
+    
